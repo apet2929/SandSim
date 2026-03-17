a344fb3e2a34d949cb15660a813fa8918c8d7796
rhit-daughewd <148395658+rhit-daughewd@users.noreply.github.com>
2026-03-17 18:07:20 -0400

    Fire Particle Created

--- src/main/java/com/apet2929/game/SandSim.java
+++ src/main/java/com/apet2929/game/SandSim.java
@@ -213,6 +213,7 @@ public class SandSim implements ILogic {
             case 2: { result = ParticleType.WATER.createParticleByMatrix(x, y); break; }
             case 3: { result = ParticleType.SMOKE.createParticleByMatrix(x, y); break; }
             case 4: { result = ParticleType.STONE.createParticleByMatrix(x, y); break; }
+            case 5: { result = ParticleType.FIRE.createParticleByMatrix(x, y); break; }
             case 0: { result = ParticleType.EMPTYPARTICLE.createParticleByMatrix(x, y); break; }
             default: { result = ParticleType.EMPTYPARTICLE.createParticleByMatrix(x, y); break; }
         }
