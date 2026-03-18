31b24e2c67163d8e515d927a9ab615be1fd3f7ec
rhit-daughewd <148395658+rhit-daughewd@users.noreply.github.com>
2026-03-18 19:46:41 -0400

    Basic Movement Tests

--- src/test/java/game/TestSandSim.java
+++ src/test/java/game/TestSandSim.java
@@ -4,7 +4,11 @@ import com.apet2929.engine.MouseInput;
 import com.apet2929.engine.RenderManager;
 import com.apet2929.engine.WindowManager;
 import com.apet2929.engine.model.ObjectLoader;
+import com.apet2929.engine.model.Grid;
 import com.apet2929.game.SandSim;
+import com.apet2929.game.World;
+import com.apet2929.game.particles.Particle;
+import com.apet2929.game.particles.ParticleType;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
@@ -31,5 +35,72 @@ public class TestSandSim {
         assertEquals(2, sim.selectedParticleType);
     }
 
+    private World world;
+
+    @Test
+    void testSpawnParticlePlacesCorrectType() {
+        Grid grid = new Grid(1, 10, 10);
+        world = new World(grid);
+
+        world.spawnParticle(ParticleType.SAND, 4, 4);
+
+        assertEquals(ParticleType.SAND, world.getAt(4, 4).getType());
+    }
+
+    @Test
+    void testSwapParticles() {
+        Grid grid = new Grid(1, 10, 10);
+        world = new World(grid);
+
+        Particle p1 = ParticleType.SAND.createParticleByMatrix(1, 1);
+        Particle p2 = ParticleType.WATER.createParticleByMatrix(2, 2);
+
+        world.setAt(1, 1, p1);
+        world.setAt(2, 2, p2);
+        world.swapParticles(1, 1, 2, 2);
+
+        assertEquals(p1, world.getAt(2, 2));
+        assertEquals(p2, world.getAt(1, 1));
+    }
+
+    @Test
+    void testMoveDown() {
+        Grid grid = new Grid(1, 10, 10);
+        world = new World(grid);
+
+        Particle p = ParticleType.SAND.createParticleByMatrix(5, 5);
+        world.setAt(5, 5, p);
+        world.moveDown(new org.joml.Vector2i(5, 5));
+
+        assertEquals(p, world.getAt(5, 4));
+    }
+
+    @Test
+    void testMoveDownAtBottom() {
+        Grid grid = new Grid(1, 10, 10);
+        world = new World(grid);
+
+        Particle p = ParticleType.SAND.createParticleByMatrix(5, 0);
+        world.setAt(5, 0, p);
+        world.moveDown(new org.joml.Vector2i(5, 0));
+
+        assertEquals(p, world.getAt(5, 0));
+    }
+
+    @Test
+    void testSwapParticle() {
+        Grid grid = new Grid(1, 10, 10);
+        world = new World(grid);
+
+        Particle p1 = ParticleType.SAND.createParticleByMatrix(5, 5);
+        Particle p2 = ParticleType.WATER.createParticleByMatrix(5, 4);
+        world.setAt(5, 5, p1);
+        world.setAt(5, 4, p2);
+
+        world.swapParticles(5, 5, 5, 4);
+
+        assertEquals(p2, world.getAt(5, 5));
+        assertEquals(p1, world.getAt(5, 4));
+    }
 
 }
