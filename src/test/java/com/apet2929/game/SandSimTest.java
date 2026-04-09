d7abfb3cde32ec92bbfc5f03df5ed840fda3ec61
rhit-daughewd <148395658+rhit-daughewd@users.noreply.github.com>
2026-04-09 11:31:04 -0400

    ParticleUpdateTest

--- src/test/java/com/apet2929/game/SandSimTest.java
+++ src/test/java/com/apet2929/game/SandSimTest.java
@@ -173,4 +173,20 @@ class SandSimTest {
         assertEquals(p1, world.getAt(5, 4));
     }
 
+    @Test
+    void testParticleUpdate(){
+        Grid grid = new Grid(1, 10, 10);
+        world = new World(grid);
+
+        Particle p1 = ParticleType.SAND.createParticleByMatrix(5, 5);
+        world.setAt(5, 5, p1);
+
+        for(int i =0; i<3; i++) {
+            world.update();
+        }
+
+        assertNotEquals(p1, world.getAt(5, 5));
+        assertTrue(p1.getGridY() < 5);
+    }
+
 }
